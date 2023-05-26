8c8440eac5785101dd1a22a9d2479b93468b3034
Visagan Shan <visagan.shan@gmail.com>
2023-05-26 14:31:57 -0400

    downloaded required dependencies

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import './HomePage.scss'
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 import VideoDetails from '../../data/video-details.json'
 import CurrentVideo from '../../components/CurrentVideo/CurrentVideo.js'
 import Description from '../../components/Description/Description.js'
@@ -8,14 +10,23 @@ import VideoList from '../../components/VideoList/VideoList.js'
 
 const HomePage = () => {
 
-    const videoList = VideoDetails; 
+    const apiUrl = "https://project-2-api.herokuapp.com";
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const videoList = VideoDetails;
   const [currentVideo, setCurrentVideo] = useState(videoList[0]);
   
   function handleVideoChange(id) {
     let newVid = videoList.find(data => data.id === id);
     setCurrentVideo(newVid);
-
   }
+
+  useEffect(()=>{
+    axios
+    .get(`${apiUrl}/videos?api_key=${apiKey}`)
+    .then(response => {
+      console.log(response.data[0]);
+    })
+  })
 
     return(
     <div className="homepage">
