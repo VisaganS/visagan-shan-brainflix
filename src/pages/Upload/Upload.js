14780606cf84ee492d173ae87e1049520ce341c5
Visagan Shan <visagan.shan@gmail.com>
2023-05-29 23:11:59 -0400

    fixed upload page styling and fixed indentation

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -8,14 +8,18 @@ const Upload = () => {
         <div className= "upload">
             <h1 className="upload__title">Upload Video</h1>
             <div className="upload__content">
-            <label className="upload__label">VIDEO THUMBNAIL<img src={uploadPreview} className ="upload__image" alt="upload-image"/></label>
             <form className="upload__form">
+                <div className="upload__form-content">
+                <label className="upload__label">VIDEO THUMBNAIL<img src={uploadPreview} className ="upload__image" alt="upload-image"/></label>
+                <div className="upload__form-inputs">
                 <label className="upload__label">TITLE YOUR VIDEO
                 <input className="upload__input-title" name="input-title" type="text" placeholder='Add a title to your video'></input>
                 </label>
                 <label className="upload__label">ADD A VIDEO DESCRIPTION
                 <textarea className="upload__input-desc" name="input-desc" type="text" placeholder='Add a description to your video'></textarea>
                 </label>
+                </div>
+                </div>
                 <div className="upload__button-container">
                     <button type="submit" className="upload__button"><img src={publishIcon} className="upload__icon"/>PUBLISH</button>
                     <button type="submit" className="upload__button upload__button--white">CANCEL</button>
